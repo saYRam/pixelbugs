1c088bebcea77194409666ee522d9d8c8bf2e3e2
andy.pike@pixeldragons.com <andy.pike@pixeldragons.com@ad23e89d-0e4d-0410-8ab8-1b34dcd0057f>
2008-05-13 20:27:01 +0000

    Added solution with basically set-up projects for tests, web application and core (domain/services etc).

---
+++ src/PixelDragons.PixelBugs.Web/GlobalApplication.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+
+using Castle.Windsor;
+using Castle.Windsor.Configuration.Interpreters;
+
+namespace PixelDragons.PixelBugs.Web
+{
+    public class GlobalApplication : HttpApplication, IContainerAccessor
+    {
+        private static IWindsorContainer _container;
+
+        public void Application_OnStart()
+        {
+            _container = new WindsorContainer(new XmlInterpreter());
+        }
+
+        public void Application_OnEnd()
+        {
+            _container.Dispose();
+        }
+
+        public IWindsorContainer Container
+        {
+            get { return _container; }
+        }
+    }
+}

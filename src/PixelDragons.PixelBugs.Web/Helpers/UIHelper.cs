664f958fc79e205d304ae898b2e739820d7959d6
andy.pike@pixeldragons.com <andy.pike@pixeldragons.com@ad23e89d-0e4d-0410-8ab8-1b34dcd0057f>
2008-05-20 22:52:34 +0000

    Extended the Issue class and added the User class. Also added UI helper and resources. See blog post 5 http://pixelbugs.wordpress.com.

---
+++ src/PixelDragons.PixelBugs.Web/Helpers/UIHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Castle.MonoRail.Framework.Helpers;
+using PixelDragons.PixelBugs.Core.Domain;
+
+namespace PixelDragons.PixelBugs.Web.Helpers
+{
+    public class UIHelper : AbstractHelper
+    {
+        public string FormatUser(User user, string nullText)
+        {
+            if (user != null)
+            {
+                return user.FullName;
+            }
+
+            return nullText;
+        }
+
+        public string FormatDate(DateTime date)
+        {
+            return date.ToString("d MMM yyyy");
+        }
+    }
+}
